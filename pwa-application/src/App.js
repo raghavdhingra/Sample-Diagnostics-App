743f663610a83729f81512209f031a735d682a0f
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-03-21 22:40:33 +0530

    simple PWA page redirections

--- pwa-application/src/App.js
+++ pwa-application/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch, Link, BrowserRouter as Router, Route } from 'react-router-dom';
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
           <h1>Home</h1>
+          <Link to="/new">New page</Link>
         </Route>
         <Route exact path="/new">
           <h1>New</h1>
+          <Link to="/">Home page</Link>
         </Route>
       </Switch>
     </Router>
